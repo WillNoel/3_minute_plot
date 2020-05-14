d62731a973217087cfa8700c414e819d759d8875
WillNoel <64814579+WillNoel@users.noreply.github.com>
2020-05-14 09:11:41 -0600

    Add files via upload
    Add final version of code and progression of images up to v5

--- May 12, 2020/turbine_size.m
+++ May 12, 2020/turbine_size.m
@@ -32,44 +32,77 @@ data   = readtable('turbine_data.csv');
 year   = data.Year;
 height = data.Height;
 diamtr = data.Diameter;
-%clear data
+rating = data.Rating;
+clear data
 
 % Find average height and diamtr for each year
 unique_year = unique(year);            % Years where new turbines built
 n            = length(unique_year);    % Number of unique years
 avg_height   = ones(n,1);              % Initialize vector size
 avg_diamtr   = ones(n,1);
-turb_count   = ones(n,1);
+inst_capac   = ones(n,1);
 
 for i = 1:1:n
-    idx = find(year==unique_year(i));    
-    avg_height(i) = mean(height(idx));
-    avg_diamtr(i) = mean(diamtr(idx));
-    turb_count(i) = nnz(idx);
+    idx = find(year==unique_year(i));
+    inst_capac(i) = sum(rating(idx));
+    % Capacity weighted averages
+    avg_height(i) = mean(rating(idx).'*height(idx))./inst_capac(i);
+    avg_diamtr(i) = mean(rating(idx).'*diamtr(idx))./inst_capac(i);
 end
+
+% Flip vectors so smaller bubbles aren't hidden by larger ones
+unique_year = flipud(unique_year);
+avg_height  = flipud(avg_height);
+inst_capac  = flipud(inst_capac);
+avg_diamtr  = flipud(avg_diamtr);
 
 % Generate scatter plot:
 %    x-axis = year
 %    y-axis = height
 %    size   = diameter
 %    color  = rating
-h = scatter(unique_year,avg_height,1,turb_count,'Filled','MarkerEdgeColor','k');
+h = scatter(unique_year,avg_height,1,inst_capac./1000,'Filled',...
+    'MarkerEdgeColor','k');
+% Axes labels and limits
 xlabel('Year','FontSize',10);
-ylabel('Hub Height (m)','FontSize',10);
+ylabel({'Capacity-Weighted';'Annual Average Hub Height (m)'},'FontSize',10);
 xlim([1990 2025]);
-ylim([0 160]);
-set(gcf,'Position',[200,500,900,300]);
-title('Average Size and Number of Builds by Year');
-clr = colorbar; ylabel(clr,'Turbines Erected per Year');
-legend('Rotor Diameter (m)', 'location', 'northwest');
-legend boxoff;
+ylim([0 200]);
+title('Average Size and Installed Capacity by Year');
+% Window size
+set(gcf,'Position',[200 500 900 300]); % [xpos ypos width height]
+% Colorbar
+clr = colorbar;
+clr.Label.String = 'Installed Capacity (MW)';
+clr.Location = 'South';
+clr.Position = [0.15 0.77 0.3 0.05]; % [xpos ypos width height]
+pos = get(clr,'Position');
+clr.Label.Position = [pos(1)+1000 pos(2)+1.5];
+set(clr, 'YAxisLocation','bottom') % tick labels to bottom
 colormap(flipud(gray));
 
-% Obtain the axes size to scale markers
+% Obtain the axes units to scale bubble size
 currentunits = get(gca,'Units');
 set(gca, 'Units', 'Points');
 axpos = get(gca,'Position');
 set(gca, 'Units', currentunits);
 markerWidth = avg_diamtr./diff(ylim)*axpos(4);
-set(h, 'SizeData', 0.25.*pi.*markerWidth.^2);
+set(h,'SizeData', 0.25.*pi.*markerWidth.^2);
+
+% Label two of the bubbles for scale
+lbl_1 = sprintf('\\oslash%2.0f m, h = %2.0f m\n           \\downarrow',...
+    avg_diamtr(n-1),avg_height(n-1));
+lbl_n = sprintf('\\oslash%3.0f m, h = %2.0f m\n            \\downarrow',...
+    avg_diamtr(1),avg_height(1));
+text(unique_year(n-1)-2.2,avg_height(n-1)+avg_diamtr(n-1)/1.27,lbl_1);
+text(unique_year(1)-2.2,avg_height(1)+avg_diamtr(1)/1.8,lbl_n);
+
+
+
+
+
+
+
+
+
 
