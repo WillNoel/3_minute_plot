a4fa947072474a165d1774927a5218b8b4fc8391
WillNoel <64814579+WillNoel@users.noreply.github.com>
2020-05-22 12:29:10 -0600

    Add files via upload

--- June 2, 2020/capacity_per_windclass.m
+++ June 2, 2020/capacity_per_windclass.m
@@ -24,7 +24,7 @@ function capacity_per_windclass
 % See also: N/A
 % Author: Will Noel
 % email: wnoel@ualberta.ca
-% May 2020; Last revision: 21-May-2020
+% May 2020; Last revision: 22-May-2020
 %------------- BEGIN CODE --------------
 
 % Read in .csv datafile, extract data, close table
@@ -35,15 +35,14 @@ capc   = data.Capacity;
 clas   = data.WindClass;
 clear data
 
-% Clear rows with zeros in capacity column
-idx = find(capc==0);
+% Clear rows with low capacity
+idx = find(capc<3);
 yr(idx)   = [];
 prov(idx) = [];
 capc(idx) = [];
 clas(idx) = [];
 
 % Separate data by province
-
 % Alberta
 AB_idx  = find(contains(prov,'AB'));
 AB_yr   = yr(AB_idx);
@@ -56,35 +55,14 @@ BC_idx  = find(contains(prov,'BC'));
 BC_yr   = yr(BC_idx);
 BC_capc = capc(BC_idx);
 BC_clas = clas(BC_idx);
-BC_colr = [6/7 6/7 6/7];
+BC_colr = [2/3 2/3 2/3];
 
 % Manitoba
 MB_idx  = find(contains(prov,'MB'));
 MB_yr   = yr(MB_idx);
 MB_capc = capc(MB_idx);
 MB_clas = clas(MB_idx);
-MB_colr = [5/7 5/7 5/7];
-
-% Nova Scotia
-NS_idx  = find(contains(prov,'NS'));
-NS_yr   = yr(NS_idx);
-NS_capc = capc(NS_idx);
-NS_clas = clas(NS_idx);
-NS_colr = [4/7 4/7 4/7];
-
-% Ontario
-ON_idx  = find(contains(prov,'ON'));
-ON_yr   = yr(ON_idx);
-ON_capc = capc(ON_idx);
-ON_clas = clas(ON_idx);
-ON_colr = [3/7 3/7 3/7];
-
-% Quebec
-QC_idx  = find(contains(prov,'QC'));
-QC_yr   = yr(QC_idx);
-QC_capc = capc(QC_idx);
-QC_clas = clas(QC_idx);
-QC_colr = [2/7 2/7 2/7];
+MB_colr = [1/3 1/3 1/3];
 
 % Saskatchewan
 SK_idx  = find(contains(prov,'SK'));
@@ -93,30 +71,61 @@ SK_capc = capc(SK_idx);
 SK_clas = clas(SK_idx);
 SK_colr = [0 0 0];
 
+% Nova Scotia
+NS_idx  = find(contains(prov,'NS'));
+NS_yr   = yr(NS_idx);
+NS_capc = capc(NS_idx);
+NS_clas = clas(NS_idx);
+NS_colr = [0 0 0];
+
+% Ontario
+ON_idx  = find(contains(prov,'ON'));
+ON_yr   = yr(ON_idx);
+ON_capc = capc(ON_idx);
+ON_clas = clas(ON_idx);
+ON_colr = [1/2 1/2 1/2];
+
+% Quebec
+QC_idx  = find(contains(prov,'QC'));
+QC_yr   = yr(QC_idx);
+QC_capc = capc(QC_idx);
+QC_clas = clas(QC_idx);
+QC_colr = [1 1 1];
+
 % Generate scatter plot:
 %    x-axis = year
 %    y-axis = wind class
 %    size   = capacity
 %    color  = province
 
-hold on
-scatter(AB_yr,AB_clas,AB_capc*2,AB_colr,'o','Filled','MarkerEdgeColor','k');
-scatter(BC_yr,BC_clas,BC_capc*2,BC_colr,'s','Filled','MarkerEdgeColor','k');
-scatter(MB_yr,MB_clas,MB_capc*2,MB_colr,'d','Filled','MarkerEdgeColor','k');
-scatter(NS_yr,NS_clas,NS_capc*2,NS_colr,'^','Filled','MarkerEdgeColor','k');
-scatter(ON_yr,ON_clas,ON_capc*2,ON_colr,'v','Filled','MarkerEdgeColor','k');
-scatter(QC_yr,QC_clas,QC_capc*2,QC_colr,'h','Filled','MarkerEdgeColor','k');
-scatter(SK_yr,SK_clas,SK_capc*2,SK_colr,'p','Filled','MarkerEdgeColor','k');
+% BC + Prarie Provinces
+subplot(2,1,1);
+scatter(AB_yr,AB_clas,AB_capc*2,AB_colr,'o','Filled','MarkerEdgeColor','k'); hold on
+scatter(BC_yr,BC_clas,BC_capc*2,BC_colr,'o','Filled','MarkerEdgeColor','k');
+scatter(MB_yr,MB_clas,MB_capc*2,MB_colr,'o','Filled','MarkerEdgeColor','k');
+scatter(SK_yr,SK_clas,SK_capc*2,SK_colr,'o','Filled','MarkerEdgeColor','k');
+% Axes labels and limits
+ylabel('Wind Class','FontSize',10);
+xlim([1998 2020]);
+ylim([0 7]);
+title('Provincial Installed Capacity by Wind Class');
+legend('AB','BC','MB','SK','location','eastoutside');
+legend boxoff;
 
-
-% Window size
-set(gcf,'Position',[200 500 900 300]); % [xpos ypos width height]
-
+% Eastern Provinces
+subplot(2,1,2);
+scatter(ON_yr,ON_clas,ON_capc*2,ON_colr,'o','Filled','MarkerEdgeColor','k'); hold on
+scatter(QC_yr,QC_clas,QC_capc*2,QC_colr,'o','Filled','MarkerEdgeColor','k');
+scatter(NS_yr,NS_clas,NS_capc*2,NS_colr,'o','Filled','MarkerEdgeColor','k'); 
 % Axes labels and limits
 xlabel('Year','FontSize',10);
 ylabel('Wind Class','FontSize',10);
-xlim([1990 2020]);
+xlim([1998 2020]);
 ylim([0 7]);
-title('Provincial Installed Capacity by Wind Class');
-legend('AB','BC','MB','NS','ON','QC','SK','location','northwest');
+legend('ON','QC','NS','location','eastoutside');
 legend boxoff;
+
+% Window size
+set(gcf,'Position',[200 200 900 600]); % [xpos ypos width height]
+
+
