6b55bb99abc8e7c6a5037116f04f51f5195d6f73
WillNoel <64814579+WillNoel@users.noreply.github.com>
2020-07-09 09:46:45 -0600

    LaTeX interpreter, change xticks and labels

--- May 19, 2020/build_per_province.m
+++ May 19, 2020/build_per_province.m
@@ -29,7 +29,7 @@ function build_per_province
 
 % Read in .csv datafile, extract data, close table
 data   = readtable('build_per_province.csv');
-year   = data.Year;
+yr     = data.Year;
 ab     = data.AB;
 bc     = data.BC;
 mb     = data.MB;
@@ -43,7 +43,7 @@ sk     = data.SK;
 clear data
 
 % Sort all data by date
-[year,idx] = sort(year);
+[yr,idx]   = sort(yr);
 ab         = ab(idx,:);
 bc         = bc(idx,:);
 mb         = mb(idx,:);
@@ -60,23 +60,39 @@ ac    = nl+nb+pei+ns;   % Atlantic Canada (maritimes)
 ep    = sk+mb;          % Eastern Praries
 
 % Generate matrix for stacked area plotting
-stack = [on qc ab ac bc ep];
+stack = [ac qc on ep ab bc];
+
+% Define tile of set size for plotting
+t = tiledlayout(1,1,'Padding','none');
+t.Units = 'inches';
+t.OuterPosition = [0.25 0.25 4 3.5]; % [xpos ypos width height]
+nexttile;
 
 % Generate stacked area plot:
 %    x-axis = year
 %    y-axis = rating
 %    color  = province
-fig = area(year,stack/1000,'EdgeColor','None');
-xlabel('Year');
-ylabel('Installed Capacity (GW)');
+fig1 = area(yr,stack/1000,'EdgeColor',[1 1 1]);
+set(gcf,'DefaultTextInterpreter','latex');
+set(gcf,'DefaultAxesTickLabelInterpreter','latex');
+set(gca,'DefaultAxesTickLabelInterpreter','latex','FontSize',11);
+xlabel('Year','FontSize',12);
+ylabel('Installed Capacity (GW)','FontSize',12);
 %title('Cumulative Installed Wind Capacity');
-legend(fliplr(fig),'EP','BC','AC','AB','QC','ON','location','northwest');
+l = legend(fliplr(fig1),'BC','AB','EP','ON','QC','AC',...
+    'location','northwest','FontSize',11);
 legend boxoff;
+set(l,'Interpreter','latex');
 xlim([1998 2019]);
+xticks([1999 2004 2009 2014 2019]);
+xticklabels({1999 2004 2009 2014 2019});
 
 % Loop through and set colormap to greyscale
-clr = [5/6 5/6 5/6];
+clr = [1 1 1];
 for i = 1:6
-    fig(i).FaceColor = clr;
-    clr = clr - 1/6;
+    fig1(i).FaceColor = clr;
+    clr = clr - [1/5 1/5 1/5];
 end
+
+% Add border to white
+fig1(1).EdgeColor = [4/5 4/5 4/5];
