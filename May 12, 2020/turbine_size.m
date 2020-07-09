1d68e619a3e22c0290fdcdb2d5ab173435d57287
WillNoel <64814579+WillNoel@users.noreply.github.com>
2020-07-09 09:39:29 -0600

    Use LaTeX interpreter, change window size

--- May 12, 2020/turbine_size.m
+++ May 12, 2020/turbine_size.m
@@ -24,7 +24,7 @@ function turbine_size
 % See also: N/A
 % Author: Will Noel
 % email: wnoel@ualberta.ca
-% May 2020; Last revision: 14-May-2020
+% May 2020; Last revision: 09-July-2020
 %------------- BEGIN CODE --------------
 
 % Read in .csv datafile, extract data, close table
@@ -40,45 +40,52 @@ unique_year = unique(year);            % Years where new turbines built
 n            = length(unique_year);    % Number of unique years
 avg_height   = ones(n,1);              % Initialize vector size
 avg_diamtr   = ones(n,1);
-inst_capac   = ones(n,1);
+avg_capac    = ones(n,1);
 
 for i = 1:1:n
     idx = find(year==unique_year(i));
-    inst_capac(i) = sum(rating(idx));
     % Averages
     avg_height(i) = mean(height(idx));
     avg_diamtr(i) = mean(diamtr(idx));
+    avg_capac(i)  = mean(rating(idx));
 end
 
 % Flip vectors so smaller bubbles aren't hidden by larger ones
 unique_year = flipud(unique_year);
 avg_height  = flipud(avg_height);
-inst_capac  = flipud(inst_capac);
 avg_diamtr  = flipud(avg_diamtr);
+avg_capac   = flipud(avg_capac);
 
 % Generate scatter plot:
 %    x-axis = year
 %    y-axis = height
 %    size   = diameter
 %    color  = rating
-h = scatter(unique_year,avg_height,1,inst_capac./1000,'Filled',...
+h = scatter(unique_year,avg_height,1,avg_capac./1000,'Filled',...
     'MarkerEdgeColor','k');
+box on;
 % Axes labels and limits
-xlabel('Year');
-ylabel({'Annual Average Hub Height (m)'});
-xlim([1990 2025]);
+set(gcf,'DefaultTextInterpreter','latex');
+set(gcf,'DefaultAxesTickLabelInterpreter','latex');
+set(gca,'DefaultAxesTickLabelInterpreter','latex','FontSize',11);
+ylabel('Annual Average Hub Height (m)','Interpreter','latex','FontSize',12);
+xlabel('Year','Interpreter','latex','FontSize',12);
+xlim([1990 2027]);
 ylim([0 200]);
-%title('Average Turbine Size and Total Installed Capacity by Year');
-% Window size
-set(gcf,'Position',[200 500 900 300]); % [xpos ypos width height]
+xticks([1990 1995 2000 2005 2010 2015 2020]);
+xticklabels({1990 1995 2000 2005 2010 2015 2020});
+% % Window size
+set(gcf,'Units','inches','Position',[1 1 7.5 3.5]); % [xpos ypos width height]
 % Colorbar
 clr = colorbar;
-clr.Label.String = 'Installed Capacity (MW)';
 clr.Location = 'South';
-clr.Position = [0.15 0.77 0.3 0.05]; % [xpos ypos width height]
-pos = get(clr,'Position');
-clr.Label.Position = [pos(1)+1000 pos(2)+1.5];
+clr.FontSize = 11;
+clr.Label.Interpreter = 'latex';
+clr.TickLabelInterpreter = 'latex';
+clr.Position = [0.175 0.8 0.33 0.02]; % [xpos ypos width height] 
+clr.Label.String = 'Average Turbine Capacity (MW)';
 set(clr, 'YAxisLocation','bottom') % tick labels to bottom
+clr.Label.Position = [1.8 3.25 0];
 colormap(flipud(gray));
 
 % Obtain the axes units to scale bubble size
@@ -90,19 +97,9 @@ markerWidth = avg_diamtr./diff(ylim)*axpos(4);
 set(h,'SizeData', 0.25.*pi.*markerWidth.^2);
 
 % Label two of the bubbles for scale
-lbl_1 = sprintf('\\oslash%2.0f m, h = %2.0f m\n           \\downarrow',...
-    avg_diamtr(n-1),avg_height(n-1));
-lbl_n = sprintf('\\oslash%3.0f m, h = %2.0f m\n            \\downarrow',...
-    avg_diamtr(1),avg_height(1));
-text(unique_year(n-1)-2.2,avg_height(n-1)+avg_diamtr(n-1)/1.27,lbl_1);
-text(unique_year(1)-2.2,avg_height(1)+avg_diamtr(1)/1.8,lbl_n);
-
-
-
-
-
-
-
-
-
-
+lbl_1 = sprintf('%2.0f m,\n h = %2.0f m',avg_diamtr(n-1),avg_height(n-1));
+lbl_n = sprintf('%3.0f m,\n h = %2.0f m',avg_diamtr(1),avg_height(1));
+text(unique_year(n-1),avg_height(n-1)+avg_diamtr(n-1)/1.4,...
+    ['$\oslash$ ' lbl_1],'FontSize',11,'HorizontalAlignment','center');
+text(unique_year(1),avg_height(1)+avg_diamtr(1)/1.8,...
+    ['$\oslash$ ',lbl_n],'FontSize',11,'HorizontalAlignment','center');
